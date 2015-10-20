8692dd7bab229a552be8b98debba0fcaed860cd4
dharmik patel <dharmikisback@gmail.com>
2015-10-20 13:34:45 +0530

    Add code for google analytics
    Add code for google analytics

---
+++ googleanalytics/googleanalytics/GAService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Android.Gms.Analytics;
+using Android.Content;
+
+namespace googleanalytics
+{
+	public class GAService
+	{
+		public string TrackingId = ""; // Place your ID here
+		private static GoogleAnalytics GAInstance;
+		private static Tracker GATracker;
+		private static GAService thisRef;
+
+		public GAService ()
+		{}
+
+		public static GAService GetGASInstance()
+		{
+			if (thisRef == null)
+				// it's ok, we can call this constructor
+				thisRef = new GAService();
+			return thisRef;
+		}
+
+		public void Initialize(Context AppContext)
+		{
+			GAInstance = GoogleAnalytics.GetInstance(AppContext.ApplicationContext);
+			GAInstance.SetLocalDispatchPeriod(10);
+
+			GATracker = GAInstance.NewTracker(TrackingId);
+			GATracker.EnableExceptionReporting(true);
+			GATracker.EnableAdvertisingIdCollection(true);
+			GATracker.EnableAutoActivityTracking(true);
+		}
+
+		public void Track_App_Page(String PageNameToTrack)
+		{
+			GATracker.SetScreenName(PageNameToTrack);
+			GATracker.Send(new HitBuilders.ScreenViewBuilder().Build());
+		}
+
+		public void Track_App_Event(String GAEventCategory, String EventToTrack)
+		{
+			HitBuilders.EventBuilder builder = new HitBuilders.EventBuilder();
+			builder.SetCategory(GAEventCategory);
+			builder.SetAction(EventToTrack);
+			builder.SetLabel("AppEvent");
+
+			GATracker.Send(builder.Build());
+		}
+
+		public void Track_App_Exception(String ExceptionMessageToTrack, Boolean isFatalException)
+		{
+			HitBuilders.ExceptionBuilder builder = new HitBuilders.ExceptionBuilder();
+			builder.SetDescription(ExceptionMessageToTrack);
+			builder.SetFatal(isFatalException);
+
+			GATracker.Send(builder.Build());
+		}
+
+		public struct GAEventCategory
+		{
+			public static String Category1 { get { return "Category1"; } set { } }
+			public static String Category2 { get { return "Category2"; } set { } }
+			public static String Category3 { get { return "Category3"; } set { } }
+		};
+	}
+}
+
---
+++ googleanalytics/googleanalytics/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Analytics;
+
+namespace googleanalytics
+{
+	[Activity (Label = "googleanalytics", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Context con = ApplicationContext;
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			GAService.GetGASInstance().Initialize(con);
+
+			GAService.GetGASInstance().Track_App_Page("Main Activity");
+
+			GAService.GetGASInstance().Track_App_Event(GAService.GAEventCategory.Category1, "hey ! Category 1 type event ocurred !");   
+
+			// Place try and catch and in your catch you need to place below line to track that error
+			//GAService.GetGASInstance().Track_App_Exception(ex.Message, false);
+		}
+
+
+	}
+}
+
+
